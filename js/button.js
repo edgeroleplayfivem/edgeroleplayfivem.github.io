ecd6014e9e1dd82e8c76927f3ada0ff3b5e1cf83
Mateo Alexandru <edgeroleplayfivem@gmail.com>
2024-07-12 14:04:30 +0200

    dawwda

---
+++ js/button.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var entraOraButton = document.getElementById('entraOraButton');
+    if (entraOraButton) {
+      entraOraButton.addEventListener('click', function(e) {
+        e.preventDefault();
+        console.log('Entra Ora button clicked');
+        window.alert('Rimani Aggiornato! 😏');
+      });
+    }
+  });
+  
